b8bb946706d641734dc323277a20f26ab19e48fb
Andrei Reznikov <andrezniksk@yandex.ru>
2022-09-10 17:50:54 +0800

    changed 1920px container's width to 1440px

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,36 +1,34 @@
 import 'paginationjs/dist/pagination.min';
 import 'paginationjs/dist/pagination.css';
 
-$(document).ready(() => {
-  function simpleTemplating(data) {
-    let html = '<ul class="pagination-list">';
-    $.each(data, (index, item) => {
-      html += `<li class="pagination-list__item">${item}</li>`;
-    });
-    html += '</ul>';
-    return html;
-  }
-
-  const paginationArray = [];
-
-  for (let i = 0; i < 180; i++) {
-    paginationArray.push(i);
-  }
-
-  $('.js-pagination-container').pagination({
-    dataSource: paginationArray,
-    autoHidePrevious: true,
-    autoHideNext: true,
-    pageRange: 1,
-    showPageNumbers: true,
-    showNavigator: false,
-    prevText: '',
-    nextText: '',
-    pageSize: 12,
-    callback: (data) => {
-      const html = simpleTemplating(data);
-
-      $('.js-data-container').html(html);
-    },
+function simpleTemplating(data) {
+  let html = '<ul class="pagination-list">';
+  $.each(data, (index, item) => {
+    html += `<li class="pagination-list__item">${item}</li>`;
   });
+  html += '</ul>';
+  return html;
+}
+
+const paginationArray = [];
+
+for (let i = 0; i < 180; i++) {
+  paginationArray.push(i);
+}
+
+$('.js-pagination-container').pagination({
+  dataSource: paginationArray,
+  autoHidePrevious: true,
+  autoHideNext: true,
+  pageRange: 1,
+  showPageNumbers: true,
+  showNavigator: false,
+  prevText: '',
+  nextText: '',
+  pageSize: 12,
+  callback: (data) => {
+    const html = simpleTemplating(data);
+
+    $('.js-data-container').html(html);
+  },
 });
