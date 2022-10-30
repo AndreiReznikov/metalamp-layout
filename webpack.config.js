c429eb3526076efce8006661134986963aa5ea60
Andrei Reznikov <andrezniksk@yandex.ru>
2022-10-30 12:16:24 +0800

    removed resolve-url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
               ],
             },
           },
-        }, 'resolve-url-loader', {
+        }, {
           loader: 'sass-loader',
           options: {
             sourceMap: true,
