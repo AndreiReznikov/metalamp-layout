c9efbbdcb918232d216875001b1e9f926b9b6377
Andrei Reznikov <andrezniksk@yandex.ru>
2022-11-20 20:22:01 +0800

    added native arrows to room-card slider; added class for small dropdown

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -35,6 +35,7 @@ class Cards {
     this.filterDateDropdown.initializePlugin({
       clearButton: true,
       keyboardNav: true,
+      range: true,
       navTitles: { days: 'MM <i>yyyy</i>' },
       prevHtml: '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/></svg>',
       nextHtml: '<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 0.984375L17.0156 9L9 17.0156L7.59375 15.6094L13.1719 9.98438H0.984375V8.01562H13.1719L7.59375 2.39062L9 0.984375Z" fill="url(#paint0_linear)"/><defs><linearGradient id="paint0_linear" x1="9" y1="-13" x2="9" y2="31" gradientUnits="userSpaceOnUse"><stop stop-color="#BC9CFF"/><stop offset="1" stop-color="#8BA4F9"/></linearGradient></defs></svg>',
@@ -59,8 +60,8 @@ class Cards {
         {
           arrows: true,
           dots: true,
-          prevArrow: '<svg class="slick-arrow slick-prev" width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 11.0928L6.01563 12.499L0.0156253 6.49902L6.01563 0.499024L7.42188 1.90527L2.82813 6.49902L7.42188 11.0928Z" fill="white"/></svg>',
-          nextArrow: '<svg class="slick-arrow slick-next" width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.578125 1.90527L1.98438 0.499024L7.98438 6.49902L1.98438 12.499L0.578125 11.0928L5.17188 6.49902L0.578125 1.90527Z" fill="white"/></svg>',
+          prevArrow: '<span class="room-card__arrow-prev"></span>',
+          nextArrow: '<span class="room-card__arrow-next"></span>',
         },
       ],
     ];
--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -52,8 +52,8 @@ class SearchRoom {
         {
           arrows: true,
           dots: true,
-          prevArrow: '<svg class="slick-arrow slick-prev" width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 11.0928L6.01563 12.499L0.0156253 6.49902L6.01563 0.499024L7.42188 1.90527L2.82813 6.49902L7.42188 11.0928Z" fill="white"/></svg>',
-          nextArrow: '<svg class="slick-arrow slick-next" width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.578125 1.90527L1.98438 0.499024L7.98438 6.49902L1.98438 12.499L0.578125 11.0928L5.17188 6.49902L0.578125 1.90527Z" fill="white"/></svg>',
+          prevArrow: '<span class="room-card__arrow-prev"></span>',
+          nextArrow: '<span class="room-card__arrow-next"></span>',
         },
       ],
     ];
