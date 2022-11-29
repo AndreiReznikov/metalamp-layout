3d621fcfa3b8dbc0b7439b2a8e27c93813ffd30e
Andrei Reznikov <andrezniksk@yandex.ru>
2022-11-29 15:26:09 +0800

    added masked-text-field initialization to the registration page

--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -1,3 +1,4 @@
+import MaskedTextField from '../../libs/inputmask/inputmask';
 import Header from '../../components/header/header';
 import '../../templates/fonts.scss';
 import './registration.scss';
@@ -6,8 +7,23 @@ class Registration {
   initializeComponents() {
     this.header = new Header();
   }
+
+  initializePlugins() {
+    this._initializeMaskedTextField();
+  }
+
+  _initializeMaskedTextField() {
+    this.maskedTextField = new MaskedTextField('.js-masked-text-field__input');
+
+    this.maskedTextField.initializePlugin({
+      placeholder: 'ДД.ММ.ГГГГ',
+      alias: 'datetime',
+      inputFormat: 'dd.mm.yyyy',
+    });
+  }
 }
 
 const registration = new Registration();
 
 registration.initializeComponents();
+registration.initializePlugins();
